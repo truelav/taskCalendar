1ff00cb56a80c12171687a9d63551a7bc1b14767
truelav <madan.serghei@yahoo.com>
2018-10-19 16:40:45 -0700

    creating weeks and month components

---
+++ src/components/Month.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Month extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div className="month">vasilica</div>;
+  }
+}
+
+export default Month;
---
+++ src/components/Week.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Week extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div className="week">vasilica</div>;
+  }
+}
+
+export default Week;
